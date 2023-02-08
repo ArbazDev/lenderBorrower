b8b32742c37321bdf0e5815dfe7b449dd97d268e
ArbazDev <arbaztariqa1gmail.com>
2023-02-08 11:22:20 +0500

    controller added

---
+++ lenderBorrower/Controllers/AppointController1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace lenderBorrower.Controllers
+{
+    public class AppointController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Details(int id)
+        {
+            return Ok("This is the id" + id);
+        }
+    }
+}
---
+++ lenderBorrower/Models/item.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata.Ecma335;
+
+namespace lenderBorrower.Models
+{
+    public class item
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Borrower { get; set; }
+    }
+}
