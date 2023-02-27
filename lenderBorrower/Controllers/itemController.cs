01ba0854f02951d74c6a9993dabb7fe719eb706f
ArbazDev <81857988+ArbazDev@users.noreply.github.com>
2023-02-27 18:03:04 +0500

    EF & Migrations-MySQL connection

---
+++ lenderBorrower/Controllers/itemController.cs
@@ -0,0 +1,14 @@
+﻿using lenderBorrower.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lenderBorrower.Controllers
+{
+    public class itemController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
