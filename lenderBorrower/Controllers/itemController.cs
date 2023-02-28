feaf7f1baa34a584a4971bd0a77b24c69082d905
ArbazDev <81857988+ArbazDev@users.noreply.github.com>
2023-02-28 16:26:45 +0500

    MVC-EF-MySQL-Dependency Indjection

--- lenderBorrower/Controllers/itemController.cs
+++ lenderBorrower/Controllers/itemController.cs
@@ -1,4 +1,5 @@
 ﻿using lenderBorrower.Data;
+using lenderBorrower.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace lenderBorrower.Controllers
@@ -6,9 +7,15 @@ namespace lenderBorrower.Controllers
     public class itemController : Controller
     {
         private readonly ApplicationDbContext _db;
+        public itemController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<item> objList = _db.items;
+            return View(objList);
         }
     }
 }
